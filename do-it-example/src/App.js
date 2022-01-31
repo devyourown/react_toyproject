2852e1c58d8c3e38695412b8799ab0615a7344e4
devyourown <hyojoon56@gmail.com>
2022-01-31 14:30:14 +0900

    study ref and event handling

--- do-it-example/src/App.js
+++ do-it-example/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import Say from './03/Say';
+import React, { Component } from 'react';
+import ScrollBox from './05/ScrollBox';
 
-const App = () => {
-  return <Say/>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref)=> this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
