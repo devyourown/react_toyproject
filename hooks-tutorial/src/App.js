5a7b8df5129e3b80e7f18bdd394dacddf2dced2e
devyourown <hyojoon56@gmail.com>
2022-02-02 00:04:37 +0900

    study hooks

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+import Info from './Info';
+import Average from './Average';
+
+function App() {
+  return (
+    <Average/>
+  );
+}
+
+export default App;
