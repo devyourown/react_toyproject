315ff49ef76e1fe9c3ccbdf1cb3962faee8f3ac4
devyourown <hyojoon56@gmail.com>
2022-02-03 17:18:35 +0900

    study CSS in react

---
+++ styling-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
+
+function App() {
+  return (
+    <StyledComponent/>
+  );
+}
+
+export default App;
